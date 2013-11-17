68b1c778d6d34563859fff4956c2c2459f2137db
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-11-17 21:38:48 +0900

    #nav がスクロールして上でピタッと止まる！

--- source/javascripts/chuork01.js
+++ source/javascripts/chuork01.js
@@ -1,3 +1,18 @@
 //= require jquery-2.0.3.min.js
 //= require mixpanel
 //= require mixpanel.key
+
+jQuery(function($){
+  var nav_offset = $("#nav").offset().top;
+  var scroll_top = 0;
+
+  $(document).scroll(function(){
+    scroll_top = $(this).scrollTop();
+
+    if (nav_offset <= scroll_top) {
+      $("#nav").addClass('fixed');
+    } else if (nav_offset >= scroll_top) {
+      $("#nav").removeClass('fixed');
+    }
+  });
+})(jQuery);
