24311e6066046f665694d6dd7765585f72d7ca3c
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-11-18 00:46:45 +0900

    jQuery ready の書き方が間違っていたので修正
    ついでにダブルクォーテーションをシングルクォーテーションに変更

--- source/javascripts/chuork01.js
+++ source/javascripts/chuork01.js
@@ -3,16 +3,16 @@
 //= require mixpanel.key
 
 jQuery(function($){
-  var nav_offset = $("#nav").offset().top;
+  var nav_offset = $('#nav').offset().top;
   var scroll_top = 0;
 
   $(document).scroll(function(){
     scroll_top = $(this).scrollTop();
 
     if (nav_offset <= scroll_top) {
-      $("#nav").addClass('fixed');
+      $('#nav').addClass('fixed');
     } else if (nav_offset >= scroll_top) {
-      $("#nav").removeClass('fixed');
+      $('#nav').removeClass('fixed');
     }
   });
-})(jQuery);
+});
